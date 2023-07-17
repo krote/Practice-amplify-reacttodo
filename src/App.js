54696c5e4983bc3fcc93ac0dd6ee1e659c48316f
krote <krote0924@gmail.com>
2023-07-18 00:41:06 +0900

    add react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Amplify, {Auth} from 'aws-amplify';
+import awsmobile from "./aws-exports";
+import { withAuthenticator } from 'aws-amplify-react';
+
+Amplify.configure(awsmobile)
+
+const signUpConfig = {
+  header: 'Sign Up',
+  hideAllDefaults: true,
+  defaultCountyCode: 1,
+  signUpField: [
+    {
+      label: 'User Name',
+      key: 'username',
+      required: true,
+      displayOrder: 1,
+      type: 'string'
+    },
+    {
+      label: 'Email',
+      key: 'email',
+      required: true,
+      displayOrder: 2,
+      type: 'string'
+    },
+    {
+      label: 'Password',
+      key: 'password',
+      required: true,
+      displayOrder: 3,
+      type: 'password'
+    }
+  ]
+}
+
+function signOut(){
+  Auth.signOut()
+  .then()
+  .catch();
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <button onClick={signOut}>Sign out</button>
+      <div>
+        Hello, World
+      </div>
+    </React.Fragment>
   );
 }
 
-export default App;
+export default withAuthenticator(App, {signUpConfig});
